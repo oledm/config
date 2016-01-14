fdb5a9c406b4f9d81f416c57a851f09587c48cba
DmitryO <masdar@list.ru>
2016-01-14 10:45:57 +0300

    Add feh_browser

--- bin/feh_browser.sh
+++ bin/feh_browser.sh
@@ -1,1 +1,4 @@
-/usr/local/bin/feh_browser.sh
+#!/bin/sh
+#feh -g 1920x1056 --title "feh [%u из %l] - %f (%wx%h, %S)" -C /home/dm/.fonts/ -M PTS55F/11 -e PTS55F/12  --action ";animate -loop 0 -delay 10 %F &" --action1 ";gthumb %F &" --action2 ";gimp %F" -. "$(dirname "$1")" --start-at "$1"
+feh -g 1920x1060 --title "feh [%u из %l] - %f (%wx%h, %S)" -C /home/dm/.fonts/ -M PTS55F/11 -e PTS55F/12  --action ";animate -loop 0 -delay 10 %F &" --action1 ";gthumb %F &" "$(dirname "$1")" --start-at "$1"
+#feh -g 1366x748 --title "feh [%u из %l] - %f (%wx%h, %S)" -C /home/dm/.fonts/ -M PTS55F/11 -e PTS55F/12  --action ";animate -loop 0 -delay 10 %F &" --action1 ";gthumb %F &" --action2 ";gimp %F"  "$(dirname "$1")" --start-at "$1"
